9481ff2eb1c4828636957b85771aaa3de07c2ec0
Priyanshu Gpta <priyanshugupta.112002@gmail.com>
2024-05-30 10:13:10 +0530

    spell

--- Server/routes/resturantRoutes.js
+++ Server/routes/resturantRoutes.js
@@ -4,10 +4,10 @@ const formidableMiddleware = require('express-formidable');
 const Router = express.Router()
 
 
-Router.post("/register-resturant" ,formidableMiddleware(),  registerResturantComtroller)
+Router.post("/register-restaurant" ,formidableMiddleware(),  registerResturantComtroller)
 
 
-Router.get("/get_All_Resturant" , get_All_resturant)
+Router.get("/get_All_Restaurant" , get_All_resturant)
 
 Router.get("/get_retrurant_Photo/:pid" , get_Retrurant_Photo)
 
