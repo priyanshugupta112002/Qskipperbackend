2d43743840a3a7db77f6c2b6d5c18b3739f9e6db
Priyanshu Gpta <priyanshugupta.112002@gmail.com>
2024-06-03 10:46:16 +0530

    update

--- Server/controller/productController.js
+++ Server/controller/productController.js
@@ -40,7 +40,7 @@ const get_All_Product = async(req,res)=>{
     const {resturant_id} = req.params;
 
     const All_Product = await ProductSchema.find({resturant:resturant_id}).select("-extraTime").sort({availability :true})
-
+    console.log(All_Product)
     // .populate({
     //     path: 'resturant',
     //     select: '-bannerPhoto',
